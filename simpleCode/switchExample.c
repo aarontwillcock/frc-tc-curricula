b96b6ada839a83da7dad58c61736f69e9d13c0cd
Aaron Willcock <aaron.willcock@wayne.edu>
2019-12-09 20:27:11 -0500

    Added simple switch example

--- simpleCode/switchExample.c
+++ simpleCode/switchExample.c
@@ -1,25 +1,12 @@
 task main()
 {
-    int a = 5;
-
+    int a = 3;
     switch(a){
         case 0:
             displayTextLine(1,"Zero");
             break;
         case 1:
             displayTextLine(1,"One");
-            break;
-        case 2:
-            displayTextLine(1,"Two");
-            break;
-        case 3:
-            displayTextLine(1,"Three");
-            break;
-        case 4:
-            displayTextLine(1,"Four");
-            break;
-        case 5:
-            displayTextLine(1,"Five");
             break;
     }
 }
