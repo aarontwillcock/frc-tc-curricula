9896f4e17074999a6b77c4d8aa9ccf6351035f2a
Aaron Willcock <aaron.willcock@wayne.edu>
2019-12-09 20:27:22 -0500

    Added switch example with a missing break

---
+++ simpleCode/nobreakSwitchExample.c
@@ -0,0 +1,12 @@
+task main()
+{
+    int a = 3;
+
+    switch(a){
+        case 0:
+            displayTextLine(1,"Zero");
+        case 1:
+            displayTextLine(2,"One");
+            break;
+    }
+}
